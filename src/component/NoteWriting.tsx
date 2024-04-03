50c20b0fd587b35cba8a59bc1616ed634217695a
sandeepyadav-24 <103882286+sandeepyadav-24@users.noreply.github.com>
2024-04-03 13:10:03 +0530

    Update conversation feature

--- src/component/NoteWriting.tsx
+++ src/component/NoteWriting.tsx
@@ -1,5 +1,3 @@
-import { Editor } from "novel";
-
 const NoteWriting = () => {
   return (
     <div className="w-4/5 m-3 bg-[#F5F9FC] rounded-xl">
@@ -9,7 +7,6 @@ const NoteWriting = () => {
         <h1 className="text-6xl font-bold">
           Just put all Your head space Here
         </h1>
-        <Editor />
       </div>
     </div>
   );
